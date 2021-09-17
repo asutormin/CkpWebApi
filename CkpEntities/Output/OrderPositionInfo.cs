83c53bf5a5bff6a90f1fbf3707ebf3dbf05d9f9a
Андрей Сутормн <ak.sutormin@gmail.com>
2021-09-17 17:31:40 +0300

    [Рефакторинг] Изменение названия цены клиента (доработка 2).

--- CkpEntities/Output/OrderPositionInfo.cs
+++ CkpEntities/Output/OrderPositionInfo.cs
@@ -6,12 +6,13 @@ namespace CkpModel.Output
     {
         public int Id { get; set; }
         public int? ParentId { get; set; }
-        public float ClientPrice { get; set; }
+        public float ClientCost { get; set; }
         public int Quantity { get; set; }
         public float ClientSum { get; set; }
         public float Nds { get; set; }
         public SupplierInfoLight Supplier { get; set; }
         public FormatInfo Format { get; set; }
+        public PriceInfo Price { get; set; }
         public IEnumerable<RubricInfoLight> Rubrics { get; set; }
         public IEnumerable<GraphicInfoLight> Graphics { get; set; }
     }
